623702c54fb74e30ae3d453327af11597041b90d
ingenierapatricia <ingenierapatricia@gmail.com>
2023-08-06 22:39:40 -0500

    Exercises of calling functions and see what is wrong in them

--- 003_workflow_basics/001_workflow_basics_exercises_pjrs.R
+++ 003_workflow_basics/001_workflow_basics_exercises_pjrs.R
@@ -11,6 +11,7 @@ sin(pi / 2)
 
 #You can create new objects with the assignment operator <-:
 x <- 3 * 4 #value stored
+x
 
 #You can combine multiple elements into a vector with c():
 primes <- c(2, 3, 5, 7, 11, 13)
@@ -28,7 +29,7 @@ primes - 1
 #This <-  can be created with ALt -
 
 #3.2 Comments
-#R will ignore anaything after #
+#R will ignore anything after #
 # create vector of primes
 primes <- c(2, 3, 5, 7, 11, 13)
 
@@ -70,3 +71,53 @@ r_rock
 
 R_rocks
 
+#3.4  Calling functions
+
+seq(from = 1, to = 10) #with TAB you can find information from the function
+
+# we can rewrite this as follows:
+  
+seq(1, 10)
+
+x <- "hello world" #Rstudio assists when typing " because these come in pair
+# when a + appears it will be because R in waiting for mor input
+
+#3.5 Exercises
+
+#1. Why does this code not work?
+  
+my_variable <- 10
+my_varıable #it is not the same ı
+
+#2.Tweak each of the following R commands so that they run correctly:
+  
+  libary(todyverse)
+  
+  library(tidyverse)
+?mpg
+ggplot(dTA = mpg) +
+  geom_point(map = aes(x = displ y = hwy)) +
+  geom_smooth(method = "lm)
+  
+  ggplot(dTA = mpg) +
+    geom_point(mapping = aes(x = displ, y = hwy)) +
+    geom_smooth(method = NULL) 
+    ?????
+    
+# 3 Press Option + Shift + K / Alt + Shift + K. What happens? 
+#How can you get to the same place using the menus?
+
+#4. Let’s revisit an exercise from the Section 2.6. 
+#Run the following lines of code. Which of the two plots is saved as mpg-plot.png? Why?
+
+my_bar_plot <- ggplot(mpg, aes(x = class)) +
+  geom_bar()
+my_scatter_plot <- ggplot(mpg, aes(x = cty, y = hwy)) +
+  geom_point()
+ggsave(filename = "mpg-plot.png", plot = my_bar_plot)
+
+#It is saved the first plot because my_bar_plot variable was the one asked for saving. 
+
+
+
+#Caused by error:! object 'displ' not found
